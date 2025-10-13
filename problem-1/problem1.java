cad2f5977de4b25a74fc7b46aec8601044c7580b
Miguel Padilla <mpadi21@mit.edu>
2025-10-13 16:19:59 -0500

    Add Java as a language and complete problems 1 and 2

---
+++ problem-1/problem1.java
@@ -0,0 +1,18 @@
+public class problem1 {
+    public static void main(String[] args) {
+        // Sum of multiples of 3, using Gauss Summation
+        int multiples_of_3_sum = (333 * (3 + 999)) / 2;
+
+    	// Sum of multiples of 5, using Gauss Summation
+        int multiples_of_5_sum = (199 * (5 + 995)) / 2;
+
+        // Sum of multiples of 15, using Gauss Summation
+        int multiples_of_15_sum = (66 * (15 + 990)) / 2;
+    
+    	// Add summations of 3 and 5 then subtract the numbers that have both as factors (multiples of 15)
+	    int multiples_of_3_and_5 = multiples_of_3_sum + multiples_of_5_sum - multiples_of_15_sum;
+
+        // Display result
+        System.out.printf("%d\n", multiples_of_3_and_5);
+    }
+}
---
+++ problem-2/problem2.java
@@ -0,0 +1,21 @@
+public class problem2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+
+        while (b < 4000000) {
+            // If even, then add fibonacci term to sum
+            if (b % 2 == 0) {
+                sum += b;
+            }
+
+            // Update fibonacci terms
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        System.out.println(sum);
+    }
+}
