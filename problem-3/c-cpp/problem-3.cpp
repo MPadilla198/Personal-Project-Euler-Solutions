a6f016d8ebbe4d50f6c19a006d86cf84f0ebd14f
Miguel Padilla <mpadi21@mit.edu>
2025-10-15 18:03:21 -0500

    Add prime sieve creation utils to C++

--- problem-3/c-cpp/problem-3.cpp
+++ problem-3/c-cpp/problem-3.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
 #include <math.h>
 
+#include "../../utils/cpp/prime_sieve.cpp"
+
+using namespace std;
+
 int main()
 {
     const long int given_composite = 600851475143L;
     const int composite_sqrt = 775147;
 
     // Create prime sieve
-    bool prime_sieve[775147];
-
-    for (int i = 0; i < composite_sqrt; i++) {
-        prime_sieve[i] = true;
-    }
-
-    // Set all composite numbered indexes in the sieve to false
-    int i = 2;
-    while (i < composite_sqrt)
-    {
-        for (int index = i * i; index > 0 && index < composite_sqrt; index += i)
-        {
-            prime_sieve[index] = false;
-        }
-
-        // Update i to the next prime exposed by the sieve
-        i++;
-        while (i < composite_sqrt && !prime_sieve[i])
-        {
-            i++;
-        }
-    }
+    bool *prime_sieve = newPrimeSieve(composite_sqrt);
 
     // Iterate sieve in reverse and mod the given composite until it equals 0
     for (int j = composite_sqrt - 1; j > 0; j--)
     {
         if (prime_sieve[j] && given_composite % ((long int) j) == 0)
         {
+            cout << j << endl;
             break;
         }
     }
+
+    delete prime_sieve;
 
     return 0;
 }
---
+++ utils/cpp/prime_sieve.cpp
@@ -0,0 +1,28 @@
+bool *newPrimeSieve(int size)
+{
+    // Create prime sieve
+    bool *prime_sieve = new bool[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        prime_sieve[i] = true;
+    }
+
+    // Set all composite numbered indexes in the sieve to false
+    int i = 2;
+    while (i < size)
+    {
+        for (int index = i * i; index > 0 && index < size; index += i)
+        {
+            prime_sieve[index] = false;
+        }
+
+        // Update i to the next prime exposed by the sieve
+        do
+        {
+            i++;
+        } while (i < size && !prime_sieve[i]);
+    }
+
+    return prime_sieve;
+}
