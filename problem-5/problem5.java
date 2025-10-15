30ceb27504851e00a4825c71b275507d917c14bd
Miguel Padilla <mpadi21@mit.edu>
2025-10-14 22:21:38 -0500

    Complete problem 5 in Java

---
+++ problem-5/problem5.java
@@ -0,0 +1,17 @@
+public class problem5 {
+    public static void main(String[] args) {
+        int[] primes = {2,3,5,7,11,13,17,19};
+        int product = 1;
+
+        for (int prime : primes) {
+            int subproduct = prime;
+
+            while (subproduct < 20) {
+                product *= prime;
+                subproduct *= prime;
+            }
+        }
+
+        System.out.println(product);
+    }    
+}
