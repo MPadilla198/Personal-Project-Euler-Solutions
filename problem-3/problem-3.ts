7aa574af2c74f7e8403fd64f683f212598ca7009
Miguel Padilla <mpadi21@mit.edu>
2025-10-15 21:01:51 -0500

    Add prime sieve creation to utils for Typescript

--- problem-3/problem-3.ts
+++ problem-3/problem-3.ts
@@ -1,23 +1,12 @@
+import { NewPrimeSieve } from './../utils/typescript/prime_sieve';
+
 const given_composite = 600851475143
 let composite_sqrt = Math.floor(Math.sqrt(given_composite)) + 1
 
 // Create prime sieve and populate to assume all numbers are prime, initially
-const prime_sieve: boolean[] = new Array(composite_sqrt).fill(true);
+const prime_sieve: boolean[] = NewPrimeSieve(composite_sqrt)
 
-let i = 2
-while (i < composite_sqrt) {
-    for (let index = i*i; index < composite_sqrt; index += i) {
-        prime_sieve[index] = false
-    }
-
-    // Update i to the next prime exposed by the sieve
-    i++
-    while (i < composite_sqrt && !prime_sieve[i]) {
-        i++
-    }
-}
-
-i = composite_sqrt - 1
+let i = composite_sqrt - 1
 for (const value of prime_sieve.reverse()) {
     if (value && given_composite % i == 0) {
         console.log(i)
---
+++ utils/typescript/prime_sieve.ts
@@ -0,0 +1,19 @@
+export function NewPrimeSieve(size: number): boolean[] {
+    // Create prime sieve and populate to assume all numbers are prime, initially
+    const prime_sieve: boolean[] = new Array<boolean>(size).fill(true);
+
+    let i = 2
+    while (i < size) {
+        for (let index = i * i; index < size; index += i) {
+            prime_sieve[index] = false
+        }
+
+        // Update i to the next prime exposed by the sieve
+        i++
+        while (i < size && !prime_sieve[i]) {
+            i++
+        }
+    }
+
+    return prime_sieve
+}
