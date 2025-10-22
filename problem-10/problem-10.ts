7cd01beb47880fc868afa3a4e393a71a098d6aac
Miguel Padilla <mpadi21@mit.edu>
2025-10-21 23:04:29 -0500

    Complete problem 10 in Typescript

---
+++ problem-10/problem-10.ts
@@ -0,0 +1,18 @@
+import { NewPrimeSieve } from './../utils/typescript/prime_sieve';
+
+
+const SIZE = 2000000
+
+// Create prime sieve
+const prime_sieve: boolean[] = NewPrimeSieve(SIZE)
+
+let i = 0
+let sum = 0
+for (const val of prime_sieve) {
+    if (i >= 2 && val) {
+        sum += i
+    }
+    i++
+}
+
+console.log(sum)
