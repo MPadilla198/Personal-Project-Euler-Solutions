1df67ce07d7cfc159cad55580e1aa51b40e7ab2b
Miguel Padilla <mpadi21@mit.edu>
2025-10-21 22:55:48 -0500

    Complete problem 10 in C++

---
+++ problem-10/c-cpp/problem-10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "../../utils/cpp/prime_sieve.cpp"
+
+int main()
+{
+
+    const int SIZE = 2000000;
+
+    // Create prime sieve
+    bool *prime_sieve = newPrimeSieve(SIZE);
+
+    long int sum = 0;
+
+    for (long int i = 2; i < SIZE; i++)
+        if (prime_sieve[i])
+            sum += i;
+
+    std::cout << sum << std::endl;
+
+    delete prime_sieve;
+
+    return 0;
+}
