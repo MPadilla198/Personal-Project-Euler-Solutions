3050e91dd3834b0cbb4d801908b1c0ddda91a9ca
Miguel Padilla <mpadi21@mit.edu>
2025-10-21 22:51:24 -0500

    Complete problem 10 in C

---
+++ problem-10/c-cpp/problem-10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "../../utils/c/prime_sieve.c"
+
+int main() {
+    const int SIZE = 2000000;
+
+    // Create prime sieve
+    unsigned char* prime_sieve = getPrimeSieve(SIZE);
+
+    long int sum = 0;
+
+    for (long int i = 2; i < SIZE; i++) {
+        if (prime_sieve[i] != 0) {
+            sum += i;
+        }
+    }
+
+    printf("%ld\n", sum);
+
+    free(prime_sieve);
+
+    return 0;
+}
