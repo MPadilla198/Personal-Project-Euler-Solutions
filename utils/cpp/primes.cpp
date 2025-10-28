929a35ea75040f982729d154801a8b2985d86e2d
Miguel Padilla <mpadi21@mit.edu>
2025-10-28 01:33:30 -0500

    Add getPrimesLessThan function to primes.cpp
    This function creates an array of all primes less than the given size

--- utils/cpp/primes.cpp
+++ utils/cpp/primes.cpp
@@ -8,6 +8,10 @@ bool *newPrimeSieve(int size)
         prime_sieve[i] = true;
     }
 
+    // 0 and 1 are not prime
+    prime_sieve[0] = false;
+    prime_sieve[1] = false;
+
     // Set all composite numbered indexes in the sieve to false
     long int i = 2;
     while (i < size)
@@ -25,4 +29,38 @@ bool *newPrimeSieve(int size)
     }
 
     return prime_sieve;
+}
+
+int countPrimes(bool *primeSieve, int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (primeSieve[i])
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+long *getPrimesLessThan(int size)
+{
+    bool *primeSieve = newPrimeSieve(size);
+
+    int primesSize = countPrimes(primeSieve, size);
+
+    long *primes = new long[primesSize];
+    int primesIndex = 0;
+
+    for (long i = 0; i < size; i++)
+    {
+        if (primeSieve[i]) {
+            primes[primesIndex] = i;
+            primesIndex++;
+        }
+    }
+
+    return primes;
 }
